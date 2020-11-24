28922c0d853665071d7d7916738b9024bfdaaec9
AlwinCrasta <alwyncrasta000@gmail.com>
2020-11-24 15:37:29 +0530

    changes

--- controllers/allUser/authController.js
+++ controllers/allUser/authController.js
@@ -3,7 +3,8 @@ const bcrypt = require('bcrypt');
 const { createToken } = require("../../middleware/createToken");
 const moment = require("moment")
 const { body, validationResult } = require('express-validator');
-const logger = require("../../logger/logger")
+const logger = require("../../logger/logger");
+const Otp = require("../../models/Otp");
 
 module.exports.register = [
     body('name').not().isEmpty(),
@@ -85,3 +86,86 @@ module.exports.login = [
         }
     }
 ]
+
+
+module.exports.sendOtp = [
+    body('phoneNumber').not().isEmpty().withMessage("phoneNumber Feild is required"),
+    
+    async (req,res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array()});
+        }
+        try {
+            const { phoneNumber} = req.body;
+            if(phoneNumber.length != 10) {
+                throw Error("Enter Valid Phone Number");
+            } else {
+                logger.info("REQUESTED TORESET PASSWORD: " + phoneNumber);
+                const user = await User.findOne({ phoneNumber });
+                if(user) {
+                    logger.info("USER FOUND: " + user);
+                    await Otp.deleteMany({userId: user._id});
+                    const otp  = await Otp.create({otpValue: "123123",userId: user._id})
+
+                    logger.info("Otp created: " + user);
+                    res.status(200).json({message: "otp sent successfully"});
+                } else { 
+                    throw Error('User Not Found');
+                }
+            }
+        }
+        catch(err) {
+            logger.error(err);
+            let error = err.message 
+            res.status(400).json({ error: error });
+        }
+    }
+]
+
+
+module.exports.verifyOtp = [
+    body('phoneNumber').not().isEmpty().withMessage("phoneNumber Feild is required"),
+    body('otpValue').not().isEmpty().withMessage("otpValue Feild is required"),
+    
+    async (req,res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array()});
+        }
+        try {
+            const { phoneNumber,otpValue} = req.body;
+            if(phoneNumber.length != 10) {
+                throw Error("Enter Valid Phone Number");
+            } else {
+                logger.info("VERIFY OTP: " + phoneNumber);
+                const user = await User.findOne({ phoneNumber });
+                if(user) {
+                    logger.info("USER FOUND: " + user); 
+                    let otp = await Otp.findOne({userId: user._id});
+                    if(otp) {
+                        logger.info("OTP PRESENT IN DB: " + otp); 
+                        if(otp.otpValue == otpValue) {
+                            await Otp.deleteMany({userId: user._id});
+                            const token = await createToken(user);
+                            res.status(200).json({ user,message: "Logged In",token});
+                        } else {
+                            throw Error("Invalid Otp value")    
+                        }
+                    } else {
+                        throw Error("Invalid Password reset call")
+                    } 
+                } else { 
+                    throw Error('User Not Found');
+                }
+            }
+        }
+        catch(err) {
+            logger.error(err);
+            let error = err.message 
+            res.status(400).json({ error: error });
+        }
+    }
+]
+
+
--- routes/allUserRoutes/authRoute.js
+++ routes/allUserRoutes/authRoute.js
@@ -5,6 +5,8 @@ const router = Router();
 
 router.post("/register", authController.register);
 router.post("/login", authController.login);
+router.post("/sendOtp", authController.sendOtp);
+router.post("/verifyOtp", authController.verifyOtp);
 
 
 module.exports = router;
