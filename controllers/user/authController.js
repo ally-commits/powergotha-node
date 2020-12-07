a02bf0c21b014398129bb144a19789b1abd7eefe
AlwinCrasta <alwyncrasta000@gmail.com>
2020-12-07 16:17:29 +0530

    changes

--- controllers/user/authController.js
+++ controllers/user/authController.js
@@ -133,45 +133,51 @@ module.exports.registerWithPhoneNumber = [
             if(phoneNumber.length != 10) {
                 throw Error("Enter Valid Phone Number");
             } else { 
-                let to = "+91" + phoneNumber;
-                logger.info("VERIFY OTP: " + to);
-                client.verify.services(sId)
-                    .verificationChecks
-                    .create({to: to, code: otpValue})
-                    .then(async verification_check => {
-                        logger.info(verification_check)
-                        logger.info(verification_check.status);
+                const newUser = User.find({phoneNumber,email});
 
-                        if(verification_check.status == "approved") {
-                            logger.info("Otp Verified")
-                            const user = await User.create({ phoneNumber,name,email,verificationType: "OTP" });
-                            if(user) {
-                                logger.info("USER FOUND: " + user);  
+                if(newUser) {
+                    res.status(400).json({ error: "User already exists"});
+                } else {
+                    let to = "+91" + phoneNumber;
+                    logger.info("VERIFY OTP: " + to);
+                    client.verify.services(sId)
+                        .verificationChecks
+                        .create({to: to, code: otpValue})
+                        .then(async verification_check => {
+                            logger.info(verification_check)
+                            logger.info(verification_check.status);
 
-                                logger.info("Phone Number:" + phoneNumber);
-                                logger.info("OTP VALUE:" + otpValue)
-                                logger.info("SID:" + sId)
+                            if(verification_check.status == "approved") {
+                                logger.info("Otp Verified")
+                                const user = await User.create({ phoneNumber,name,email,verificationType: "OTP" });
+                                if(user) {
+                                    logger.info("USER FOUND: " + user);  
 
-                                const token = await createToken(user);
-                                res.json({message:"User registered Successfully",token});
-                            } else { 
-                                res.status(400).json({ error: "Connot register User"});
+                                    logger.info("Phone Number:" + phoneNumber);
+                                    logger.info("OTP VALUE:" + otpValue)
+                                    logger.info("SID:" + sId)
+
+                                    const token = await createToken(user);
+                                    res.json({message:"User registered Successfully",token});
+                                } else { 
+                                    res.status(400).json({ error: "Connot register User"});
+                                }
+                            } else {
+                                logger.error("Invalid Otp:" + verification_check.status)
+                                res.status(400).json({ error: "Invalid Otp Value"});
                             }
-                        } else {
-                            logger.error("Invalid Otp:" + verification_check.status)
-                            res.status(400).json({ error: "Invalid Otp Value"});
-                        }
-                    }).catch(err => {
-                        logger.error(err)
-                        console.log("-----------------")
-                        console.log(err)
-                        console.log(err.code)
-                        let error = err.message 
-                        if(err.code == 11000) {
-                            error = Object.keys(err.keyValue)[0] + " already exists"
-                        }
-                        res.status(400).json({ error: error});
-                    })
+                        }).catch(err => {
+                            logger.error(err)
+                            console.log("-----------------")
+                            console.log(err)
+                            console.log(err.code)
+                            let error = err.message 
+                            if(err.code == 11000) {
+                                error = Object.keys(err.keyValue)[0] + " already exists"
+                            }
+                            res.status(400).json({ error: error});
+                        })
+                }
             }
         }
         catch(err) {
